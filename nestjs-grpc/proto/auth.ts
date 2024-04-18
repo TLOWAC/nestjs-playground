ba057a34c57b0b06dafb35d6c23b3e1bd6612367
TLOWAC <leechanghun55@gmail.com>
2024-04-18 19:56:33 +0900

    chore(generated): auth protobuf -> ts 생성

---
+++ nestjs-grpc/proto/auth.ts
@@ -0,0 +1,104 @@
+/* eslint-disable */
+import { GrpcMethod, GrpcStreamMethod } from "@nestjs/microservices";
+import { Observable } from "rxjs";
+
+export const protobufPackage = "auth";
+
+/** CreateUserDto 형식 정의 */
+export interface CreateUserDto {
+  username: string;
+  password: string;
+  age: number;
+}
+
+/** FindOneUserDto 형식 정의 */
+export interface FindOneUserDto {
+  id: string;
+}
+
+/** UpdateUserDto 형식 정의 */
+export interface UpdateUserDto {
+  id: string;
+  socialMedia: SocialMedia | undefined;
+}
+
+/** PaginationDto 형식 정의 */
+export interface PaginationDto {
+  page: number;
+  size: number;
+}
+
+/** input/output 이 없는 경우의 형식 정의 */
+export interface Empty {
+}
+
+/** 전송할 메시지 형식 정의 -> Users */
+export interface Users {
+  users: User[];
+}
+
+/** 전송할 메시지 형식 정의 -> User */
+export interface User {
+  id: string;
+  username: string;
+  password: string;
+  age: number;
+  subscribed: boolean;
+  socialMedia: SocialMedia | undefined;
+}
+
+export interface SocialMedia {
+  twitterUri?: string | undefined;
+  facebookUri?: string | undefined;
+}
+
+export const AUTH_PACKAGE_NAME = "auth";
+
+/** 서비스 메서드 정의 */
+
+export interface UsersServiceClient {
+  createUser(request: CreateUserDto): Observable<User>;
+
+  findAllUsers(request: Empty): Observable<Users>;
+
+  findOneUser(request: FindOneUserDto): Observable<User>;
+
+  updateUser(request: UpdateUserDto): Observable<User>;
+
+  removeUser(request: FindOneUserDto): Observable<User>;
+
+  queryUsers(request: Observable<PaginationDto>): Observable<Users>;
+}
+
+/** 서비스 메서드 정의 */
+
+export interface UsersServiceController {
+  createUser(request: CreateUserDto): Promise<User> | Observable<User> | User;
+
+  findAllUsers(request: Empty): Promise<Users> | Observable<Users> | Users;
+
+  findOneUser(request: FindOneUserDto): Promise<User> | Observable<User> | User;
+
+  updateUser(request: UpdateUserDto): Promise<User> | Observable<User> | User;
+
+  removeUser(request: FindOneUserDto): Promise<User> | Observable<User> | User;
+
+  queryUsers(request: Observable<PaginationDto>): Observable<Users>;
+}
+
+export function UsersServiceControllerMethods() {
+  return function (constructor: Function) {
+    const grpcMethods: string[] = ["createUser", "findAllUsers", "findOneUser", "updateUser", "removeUser"];
+    for (const method of grpcMethods) {
+      const descriptor: any = Reflect.getOwnPropertyDescriptor(constructor.prototype, method);
+      GrpcMethod("UsersService", method)(constructor.prototype[method], method, descriptor);
+    }
+    const grpcStreamMethods: string[] = ["queryUsers"];
+    for (const method of grpcStreamMethods) {
+      const descriptor: any = Reflect.getOwnPropertyDescriptor(constructor.prototype, method);
+      GrpcStreamMethod("UsersService", method)(constructor.prototype[method], method, descriptor);
+    }
+  };
+}
+
+export const USERS_SERVICE_NAME = "UsersService";
