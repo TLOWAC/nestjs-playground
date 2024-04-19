3ce37d7bd2e4982d80925ccbc830758255b9619b
TLOWAC <leechanghun55@gmail.com>
2024-04-19 20:55:51 +0900

    refactor(auth): service  리팩토링

--- nestjs-grpc/apps/auth/src/users/users.service.ts
+++ nestjs-grpc/apps/auth/src/users/users.service.ts
@@ -1,26 +1,73 @@
-import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateUserDto, UpdateUserDto, User, Users } from '@app/common';
+import { randomUUID } from 'crypto';
+import { PaginationDto } from '../../../../libs/common/src/types/auth';
+import { Observable, Subject } from 'rxjs';
 
 @Injectable()
 export class UsersService {
+  private readonly users: User[] = [];
+
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    const user: User = {
+      ...createUserDto,
+      subscribed: false,
+      socialMedia: {},
+      id: randomUUID(),
+    };
+
+    this.users.push(user);
+    return user;
   }
 
-  findAll() {
-    return `This action returns all users`;
+  findAll(): Users {
+    return { users: this.users };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  findOne(id: string): User {
+    return this.users.find((user) => user.id === id);
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  update(id: string, updateUserDto: UpdateUserDto) {
+    const userIndex = this.users.findIndex((user) => user.id === id);
+    if (userIndex !== -1) {
+      this.users[userIndex] = {
+        ...this.users[userIndex],
+        ...updateUserDto,
+      };
+      return this.users[userIndex];
+    }
+    throw new NotFoundException(`User not found by id ${id}`);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  remove(id: string) {
+    const userIndex = this.users.findIndex((user) => user.id === id);
+    if (userIndex !== -1) {
+      return this.users.splice(userIndex)[0];
+    }
+    throw new NotFoundException(`User not found by id ${id}`);
+  }
+
+  queryUsers(
+    paginationDtoStream: Observable<PaginationDto>,
+  ): Observable<Users> {
+    const subject = new Subject<Users>();
+
+    const onNext = (paginationDto: PaginationDto) => {
+      const start = paginationDto.page * paginationDto.skip;
+      subject.next({
+        users: this.users.slice(start, start + paginationDto.skip),
+      });
+    };
+
+    const onComplete = () => subject.complete();
+
+    // paginationDtoStream 데이터를 구독.
+    // paginationDtoStream 를 onNext() 의 인자로 사용.
+    // 스트림이 완료되면 onComplete() 호출.
+    paginationDtoStream.subscribe({ next: onNext, complete: onComplete });
+
+    // 외부에서 사용할 수 있도록 Observable 형태로 반환
+    return subject.asObservable();
   }
 }
