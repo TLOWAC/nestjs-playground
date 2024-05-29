19cff4d40b616c22456095e77bc9b5fbb9b227e8
TLOWAC <leechanghun55@gmail.com>
2024-05-29 17:24:02 +0900

    refactor(user): 테스트용 엔드포인트 추가

--- nestjs-drizzle/src/user/user.controller.ts
+++ nestjs-drizzle/src/user/user.controller.ts
@@ -1,8 +1,12 @@
-import { Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { UserService } from './user.service';
 
+@Controller('user')
 export class UserController {
-  @Get('')
-  getUsers() {
-    return 'Hello World';
+  constructor(private readonly userService: UserService) {}
+  @Get(':id')
+  async getUser(@Param('id') id: number) {
+    const user = await this.userService.findById(id);
+    return user;
   }
 }
